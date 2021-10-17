9df71f1318e302eebe89500dd23e65bc18309c3b
Omkar Athavale <oa1234@live.com>
2021-10-18 08:33:56 +1300

    diverged error

--- test/TestImtiaz3D.hpp
+++ test/TestImtiaz3D.hpp
@@ -270,7 +270,7 @@ public:
         HeartConfig::Instance()->SetSimulationDuration(20000); //ms for one cycle 10,000
         HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.1, 1, 10); //timesteps: ode, pde, printing
         HeartConfig::Instance()->SetSurfaceAreaToVolumeRatio(2000); // Ratio for each cell
-        HeartConfig::Instance()->SetUseAbsoluteTolerance(2e-4); //Changed to get around the DIVERGED_ITS error default:2e-4
+        HeartConfig::Instance()->SetUseAbsoluteTolerance(2e-3); //Changed to get around the DIVERGED_ITS error default:2e-4
         HeartConfig::Instance()->SetCapacitance(3); // Membrane Capacitance
         HeartConfig::Instance()->SetBathConductivity(0.02); // Bath capacitance
 
